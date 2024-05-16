52016c082326557a6ebf71841b346db0508c96bd
Rohith Venkatesan <rohithvenkatesan142@gmail.com>
2024-05-16 13:45:51 -0400

    current stuff

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,37 @@
 use ethers::signers::LocalWallet;
-use hyperliquid_rust_sdk::{
-     AssetMeta, BaseUrl, ClientCancelRequest, ClientLimit, ClientOrder, ClientOrderRequest, ExchangeClient, ExchangeDataStatus, ExchangeResponseStatus, InfoClient, Message, Meta, Subscription
-};
 use tokio::sync::mpsc::unbounded_channel;
-use std::{f64, sync::{Arc, Mutex}, thread::sleep, time::Duration};
-
 use std::collections::VecDeque;
 use reqwest::Client;
 use serde::{Deserialize, Serialize};
+use chrono::Utc;
+
+use hyperliquid_rust_sdk::{
+    AssetMeta, BaseUrl, ClientCancelRequest, ClientLimit, ClientOrder, ClientOrderRequest, ExchangeClient, ExchangeDataStatus, ExchangeResponseStatus, InfoClient, Message, Meta, Subscription
+};
+use std::{
+    f64,
+    sync::{Arc, Mutex},
+    thread::sleep,
+    time::Duration
+};
+
+#[derive(Serialize)]
+struct RequestPayload {
+    #[serde(rename = "type")]
+    type_field: String,
+    user: String,
+}
+
+#[derive(Deserialize)]
+struct ResponseData {
+    balances: Vec<Balance>,
+}
+
+#[derive(Deserialize)]
+struct Balance {
+    coin: String,
+    total: String, // Using String to handle potential floating-point inconsistencies.
+}
 
 struct PriceQueue {
     queue: VecDeque<f64>,
@@ -34,8 +58,7 @@ impl PriceQueue {
         self.current_sum += new_price;
         let avg = self.current_average();
 
-
-        if new_price > avg * 1.02 {
+        if new_price > avg * 1.015 {
             (avg, "sell".to_string())
         } else if new_price < avg * 0.99 {
             (avg, "buy".to_string())
@@ -76,25 +99,6 @@ lazy_static::lazy_static! {
     };
 }
 
-#[derive(Serialize)]
-struct RequestPayload {
-    #[serde(rename = "type")]
-    type_field: String,
-    user: String,
-}
-
-#[derive(Deserialize)]
-struct ResponseData {
-    balances: Vec<Balance>,
-}
-
-#[derive(Deserialize)]
-struct Balance {
-    coin: String,
-    total: String, // Using String to handle potential floating-point inconsistencies.
-}
-
-
 async fn get_position(ticker: &str) -> Option<f64> {
     let client = Client::new();
     let ticker = ticker.to_string();
@@ -124,15 +128,12 @@ async fn get_position(ticker: &str) -> Option<f64> {
     None
 }
 
-
-
 async fn place_trade(exchange_client: &mut ExchangeClient, is_buy: bool, limit_px: f64) -> Option<u64> {
-
     let current_purr_position = get_position("PURR").await.unwrap_or(0.0);
     let current_usdc_position = get_position("USDC").await.unwrap_or(0.0);
 
     // already have that position
-    if is_buy && current_usdc_position < 100.0 || !is_buy && current_purr_position < 100.0{
+    if is_buy && current_usdc_position < 100.0 || !is_buy && current_purr_position < 100.0 {
         return None;
     }
 
@@ -157,8 +158,8 @@ async fn place_trade(exchange_client: &mut ExchangeClient, is_buy: bool, limit_p
         ExchangeResponseStatus::Ok(data) => data,
         ExchangeResponseStatus::Err(e) => {
             eprintln!("error with exchange response: {e}");
-            return None; 
-        },
+            return None;
+        }
     };
 
     // Extract the status from the response data
@@ -171,7 +172,7 @@ async fn place_trade(exchange_client: &mut ExchangeClient, is_buy: bool, limit_p
         Some(status) => {
             eprintln!("Unhandled status: {status:?}");
             None
-        },
+        }
         None => {
             eprintln!("Status list is empty");
             None
@@ -190,11 +191,10 @@ async fn cancel_trade(exchange_client: &mut ExchangeClient, oid: u64) -> bool {
         ExchangeResponseStatus::Ok(_data) => true,
         ExchangeResponseStatus::Err(e) => {
             eprintln!("error with exchange response: {e}");
-            false 
-        },
+            false
+        }
     }
 }
-
 
 #[tokio::main]
 async fn main() {
@@ -208,17 +208,14 @@ async fn main() {
         .await
         .unwrap();
 
-    
-
     let exchange_client: ExchangeClient = ExchangeClient::new(None, WALLET.clone(), Some(BaseUrl::Mainnet), Some(META.clone()), None)
-    .await
-    .unwrap();
+        .await
+        .unwrap();
 
     let shared_client = Arc::new(Mutex::new(exchange_client));
 
     let mut client = shared_client.lock().unwrap();
     let mut last_trade_action = "none".to_string();
-
 
     while let Some(Message::AllMids(all_mids)) = receiver.recv().await {
         if let Some(purr_price) = all_mids.data.mids.get("PURR/USDC") {
@@ -226,6 +223,8 @@ async fn main() {
                 Ok(new_price) => {
                     let (_average, decision) = price_queue.update_and_decide(new_price);
                     let new_price = (new_price * 100000.0).round() / 100000.0;
+                    let current_time = Utc::now();
+                    println!("Current Price: {}, Average: {}, Time: {}", new_price, _average, current_time);
 
                     match decision.as_str() {
                         "buy" | "sell" => {
@@ -233,35 +232,32 @@ async fn main() {
                             if last_trade_action == decision {
                                 continue;
                             }
-                            
+
                             println!("Decision: {decision}, Price: {new_price}");
                             let oid = place_trade(&mut client, decision == "buy", new_price).await;
                             if let Some(oid) = oid {
                                 println!("{} order placed with oid: {}", decision, oid);
-                                sleep(Duration::from_secs(10));
+                                sleep(Duration::from_secs(30));
 
                                 let cancel_response = cancel_trade(&mut client, oid).await;
 
                                 if !cancel_response {
                                     println!("{} order filled completely for oid: {}", decision, oid);
                                     last_trade_action = decision.to_string();
-                                }
-                                else{
+                                } else {
                                     let new_trade_price = if decision == "buy" { (new_price * 1.01 * 100000.0).round() / 100000.0 } else { (new_price * 0.99 * 100000.0).round() / 100000.0 };
                                     // theoretically should always fill
                                     place_trade(&mut client, decision == "buy", new_trade_price).await;
                                     last_trade_action = decision.to_string();
                                 }
-                            }
-                            else{
-                                // when the trade couldnt be placed, meaning it had already been bought/sold
+                            } else {
+                                // when the trade couldn't be placed, meaning it had already been bought/sold
                                 last_trade_action = decision.to_string();
                             }
-                        },
-                        _ => {
                         }
+                        _ => {}
                     }
-                },
+                }
                 Err(_) => {
                     eprintln!("Failed to parse PURR price");
                     continue;
